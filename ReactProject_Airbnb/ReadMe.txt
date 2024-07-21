de3b89c44fd9ae54221af36fe7f15792e254d56d
HalilDayi <120185611+HalilDayi@users.noreply.github.com>
2024-07-21 19:55:03 +0300

    React_Project_Airbnb
    data file and readMe file added.

---
+++ ReactProject_Airbnb/ReadMe.txt
@@ -0,0 +1,3 @@
+This is static example of Mini React Project.
+
+In this project, you can see how the data is pulled from the data.js file and passed to the component as props.
---
+++ ReactProject_Airbnb/src/data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id: 1,
+        img: "images/swimming.png",
+        location: "USA",
+        cardInfo: "Life lessons with Katie Zaferes",
+        stats: {
+            star: '5.0',
+            view: 6
+        },
+        price: 136,
+        availableCapac: 0,
+        isOnline: true
+    },
+    {
+        id: 2,
+        img: "images/wedding.png",
+        location: "USA",
+        cardInfo: "Learn wedding photography",
+        stats: {
+            star: '5.0',
+            view: 30
+        },
+        price: 125,
+        availableCapac: 20,
+        isOnline: true 
+    },
+    {
+        id: 3,
+        img: "images/bike.png",
+        location: "USA",
+        cardInfo: "Learn wedding photography",
+        stats: {
+            star: '4.8',
+            view: 2
+        },
+        price: 50,
+        availableCapac: 10,
+        isOnline: false
+    }
+]
